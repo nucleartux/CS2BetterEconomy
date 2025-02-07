e91084b337efe1229b3011f5f675be7ca7812d35
Adrov Igor <nucleartux@gmail.com>
2025-02-07 20:31:57 +0100

    Revert "increase industrial efficiency"
    This reverts commit 1c600a826fa8ddcf330ed49a58411970ec0c5971.

--- BetterEconomy/Mod.cs
+++ BetterEconomy/Mod.cs
@@ -39,9 +39,6 @@ namespace BetterEconomy
 
             World.DefaultGameObjectInjectionWorld.GetOrCreateSystemManaged<ResourceBuyerSystem>().Enabled = false;
             updateSystem.UpdateAt<ModifiedResourceBuyerSystem>(SystemUpdatePhase.GameSimulation);
-
-            PrefabPatcher prefabPatcher = new PrefabPatcher();
-            prefabPatcher.PatchEconomyParameters();
          }
 
         public void OnDispose()
